d86f07805e0de5ceaedee1aeb40d6311ad126e6a
Artamonov Petya <petr.artamonov@abbyy.com>
2019-08-02 17:33:57 +0300

    text
    Signed-off-by: Artamonov Petya <petr.artamonov@abbyy.com>

--- app/src/main/java/com/example/test1/ViewBatteryActivity.kt
+++ app/src/main/java/com/example/test1/ViewBatteryActivity.kt
@@ -3,11 +3,13 @@ package com.example.test1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ViewBatteryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_view_battery)
+
+
 
     }
 }
